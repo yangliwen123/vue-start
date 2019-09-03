6408ddc2661ab474d81487305dbfd906ca8af101
yangliwen123 <2782385499@qq.com>
2019-09-03 15:57:07 +0800

    文档页面添加对话框表格操作2

--- src/views/About1.vue
+++ src/views/About1.vue
@@ -1,53 +1,66 @@
 <template>
-    <div>
-      <el-table
-        :data="tableData"
-        border
-        style="width: 100%">
-        <el-table-column
-          fixed
-          prop="date"
-          label="日期"
-          width="150">
-        </el-table-column>
-        <el-table-column
-          prop="name"
-          label="姓名"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="province"
-          label="省份"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="city"
-          label="市区"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          fixed="right"
-          label="操作"
-          width="100">
-          <template slot-scope="scope">
-            <el-button @click="dialogVisible = true" type="text" size="small">编辑</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <edit :dialogVisible="dialogVisible" v-if="dialogVisible"></edit>
-    </div>
+  <div>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 80%"
+    >
+      <el-table-column
+        fixed
+        prop="date"
+        label="日期"
+        width="150"
+      />
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="120"
+      />
+      <el-table-column
+        prop="province"
+        label="省份"
+        width="120"
+      />
+      <el-table-column
+        prop="city"
+        label="市区"
+        width="120"
+      />
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100"
+      >
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="get(scope.row)">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-dialog title="更改" :visible.sync="dialogFormVisible">
+      <el-form ref="formVisible" :model="form" class="custom-form">
+        <el-form-item label="姓名" :label-width="formLabelWidth">
+          <el-input v-model="form.name" auto-complete="off" />
+        </el-form-item>
+        <el-form-item label="地址" :label-width="formLabelWidth">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai" />
+            <el-option label="区域二" value="beijing" />
+          </el-select>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogClose()">取 消</el-button>
+        <el-button type="primary" @click="confirm()">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
-import edit from './edit.vue'
 export default {
-  name: 'test',
-  components: {
-    edit
-  },
-  data () {
+  data() {
     return {
-      dialogVisible: true,
+      dialogFormVisible: false,
       tableData: [{
         date: '2016-05-03',
         name: '王小虎',
@@ -76,13 +89,37 @@ export default {
         city: '普陀区',
         address: '上海市普陀区金沙江路 1518 弄',
         zip: 200333
-      }]
+      }],
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: 'false',
+        type: [],
+        resource: '',
+        desc: ''
+      }
     }
   },
+
   methods: {
-    gougou (row) {
-      console.log(row)
-      this.dialogVisible = true
+    // 打开对话框
+    dialogOpen(row) {
+      this.form.name = row.name
+      // 把当前的row值给对话框对应的name
+      this.dialogFormVisible = true
+      console.log(row, this.form)
+    },
+    // 关闭对话框
+    dialogClose() {
+      console.log('dialogClose before', this.dialogFormVisible)
+      this.dialogFormVisible = false
+      console.log('dialogClose after', this.dialogFormVisible)
+    },
+    confirm() {
+      console.log('confirm')
+      this.dialogFormVisible = false
     }
   }
 }
